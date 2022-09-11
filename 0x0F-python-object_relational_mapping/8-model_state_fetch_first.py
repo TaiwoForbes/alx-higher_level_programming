d1399c2891efe95307f15153761ee2095487aa9c
root <soyemihissa2000@gmail.com>
2022-09-11 12:49:21 -0700

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""A script that lists all State objects from the database hbtn_0e_6_usa"""
-from sqlalchemy import create_engine
+"""
+A script that prints the first State object from the database hbtn_0e_6_usa
+"""
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 import sys
 
 from model_state import Base, State
+
 
 if __name__ == "__main__":
     engine = create_engine("mysql://{}:{}@localhost:3306/{}"
                            .format(*sys.argv[1:]))
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
-    records = session.query(State).all()
-    for record in records:
+    record = session.query(State).first()
+    if record is not None:
         print("{}: {}".format(record.id, record.name))
+    else:
+        print("Nothing")
