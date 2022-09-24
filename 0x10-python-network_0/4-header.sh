41126723fa6bf2fca11a6a1aa5d9fa117d3276e5
TaiwoForbes <soyemihissa2000@gmail.com>
2022-09-24 03:52:14 -0700

    correction on task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# curl sends GET req to URL, displays response body
-curl -sH "X-School-User-Id: 98" "$1"
+# Sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "${1}"
